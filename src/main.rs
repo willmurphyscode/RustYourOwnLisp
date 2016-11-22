cc2df9bc2c747f6aba138e3d34ab61ce30493673
William Murphy <willmurphyscode@gmail.com>
2016-11-21 19:07:03 -0500

    add module imports on main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 extern crate regex;
 use regex::Regex;
+use name_types::CoolAdjective; 
 
 mod opcode;
 mod tokenize;
+
+pub mod ints; 
+
+pub mod scientific_names;
+pub mod name_types;
+
+#[test]
+fn calculator1() {
+    assert!(ints::parse_Term("22").is_ok());
+    assert!(ints::parse_Term("(22)
+    ").is_ok());
+    assert!(ints::parse_Term("((((22))))").is_ok());
+    assert!(ints::parse_Term("((22)").is_err());
+}
+
+#[test]
+fn cool_adjective_test() {
+    assert!(scientific_names::parse_coolAdjective("horrendous").is_ok());
+}
 
 fn main() {
     println!("Hello, world!");
